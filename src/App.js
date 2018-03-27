26bd01d7bde1e735519e8a247fe39c5e75adb73e
Ruth Ashleigh <notruth@notruth.com>
2018-03-26 20:16:10 -0700

    change class name back to App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Hello extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ class Hello extends Component {
   }
 }
 
-export default Hello;
+export default App;
