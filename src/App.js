7343be4fe8554882a2a6008eff074d018ecd2553
Ruth Ashleigh <notruth@notruth.com>
2018-03-27 14:35:54 -0700

    Added some dummy data for testing and changed how state data is organised

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { MyButton } from './Button';
 import { User } from './User';
 import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 import './bootstrap.min.css';
@@ -8,22 +9,18 @@ class List extends Component {
   constructor(props){
     super(props);
 
-    this.state ={
-      chosenUser:[
-        {
-          userName: [],
-          userIcon: [],
-          userPoints: [],
-          userPointsRecent: []
-        }
-      ]
+    this.state = {
+          userName: ["roo", "beh", "cas", "mum", "dad"],
+          userIcon: ["ash", "goo", "gas", "cor", "ste"],
+          userPoints: [30, 40, 20, 50, 10],
+          userPointsRecent: [5, 2, 20, 20, 15]
     };
 
-    this.handleUserInput = this.handleUserInput.bind(this);
-    
+    this.handleClick = this.handleClick.bind(this);
+
   }
 
-  handleUserInput(e){
+  handleClick(e){
     this.setState({
       //this needs to be set and sort based on button click
     });
@@ -31,19 +28,20 @@ class List extends Component {
   renderUsers(i) {
   return (
     <User
-      userName={this.userName[i]}
-      userIcon={this.userIcon[i]}
-      userPoints={this.userPoints[i]}
-      userPointsRecent={this.userPointsRecent[i]}
+      userName={this.state.userName[i]}
+      userIcon={this.state.userIcon[i]}
+      userPoints={this.state.userPoints[i]}
+      userPointsRecent={this.state.userPointsRecent[i]}
     />
   );
 }
 
   render() {
     return (
-      <div>{ this.renderUsers(0) }</div>
-
-      //Array.from(Array(10), (_, x) => this.renderUsers[x])
+      <div>
+        {Array.from(Array(5), (_, x) => this.renderUsers(x))}
+        <MyButton />
+      </div>
     );
   }
 }
