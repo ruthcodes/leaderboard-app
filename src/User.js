a1b4bd7144328a8f531d414cda0417eee0eb0e95
Ruth Ashleigh <notruth@notruth.com>
2018-03-28 16:22:08 -0700

    Adjusted column positioning

--- src/User.js
+++ src/User.js
@@ -11,10 +11,10 @@ export class User extends Component{
     return (
       <Grid>
         <Row className="allUsers">
-          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Name:{this.props.userName}</Col>
-          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Icon: <img src={this.props.userIcon}/></Col>
-          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Total points: {this.props.userPoints}</Col>
-          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Points this month: {this.props.userPointsRecent}</Col>
+          <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}><img src={this.props.userIcon}/></Col>
+          <Col className="userDetails" xs={6} sm={6} md={6} lg={6} xl={6}>{this.props.userName}</Col>
+          <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}>Total: {this.props.userPoints}</Col>
+          <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}>Recent: {this.props.userPointsRecent}</Col>
         </Row>
       </Grid>
     );
