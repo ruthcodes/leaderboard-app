fd1eb551b676d6a62e95a4b10efbc4c6ec92a150
Ruth Ashleigh <notruth@notruth.com>
2018-03-28 16:39:37 -0700

    Corrected column ordering for user details

--- src/User.js
+++ src/User.js
@@ -12,8 +12,8 @@ export class User extends Component{
       <Grid>
         <Row className="allUsers">
           <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}><img src={this.props.userIcon}/></Col>
-          <Col className="userDetails" xs={6} sm={6} md={6} lg={6} xl={6}>{this.props.userName}</Col>
-          <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}>Total: {this.props.userPoints}</Col>
+          <Col className="userDetails" xs={6} sm={2} smPush={6} md={2} mdPush={6} lg={2} lgPush={6} xl={2} xlPush={6}>Total: {this.props.userPoints}</Col>
+          <Col className="userDetails" xs={6} sm={6} smPull={2} md={6} mdPull={2} lg={6} lgPull={2} xl={6} xlPull={2}>{this.props.userName}</Col>
           <Col className="userDetails" xs={6} sm={2} md={2} lg={2} xl={2}>Recent: {this.props.userPointsRecent}</Col>
         </Row>
       </Grid>
