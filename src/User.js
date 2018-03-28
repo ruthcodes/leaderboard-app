28c4fdf7d0c2a1aab7faee9c0ddeec5b5fc4591a
Ruth Ashleigh <notruth@notruth.com>
2018-03-28 12:50:09 -0700

    added <img> to render icons, gave styling

--- src/User.js
+++ src/User.js
@@ -12,7 +12,7 @@ export class User extends Component{
       <Grid>
         <Row className="allUsers">
           <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Name:{this.props.userName}</Col>
-          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Icon: {this.props.userIcon}</Col>
+          <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Icon: <img src={this.props.userIcon}/></Col>
           <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Total points: {this.props.userPoints}</Col>
           <Col className="userDetails" xs={12} sm={6} md={3} lg={3} xl={3}>Points this month: {this.props.userPointsRecent}</Col>
         </Row>
