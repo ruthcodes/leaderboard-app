ae3f799f8d9ad4b5e1a428e321b8331d8f8a0084
Ruth Ashleigh <notruth@notruth.com>
2018-03-28 12:20:01 -0700

    Add outline willMount(). Remove sort() as API returns data pre-sorted

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ class List extends Component {
     super(props);
 
     this.state = {
-      myUser: [
-          {userName: ["roo", "beh", "cas", "mum", "dad"]},
-          {userIcon: ["ash", "goo", "gas", "cor", "ste"]},
-          {userPoints: [30, 40, 20, 50, 10]},
-          {userPointsRecent: [5, 2, 20, 20, 15]}
-        ]
+      userName: [],
+      userIcon: [],
+      userPoints: [],
+      userPointsRecent: []
     };
 
     this.handleClick = this.handleClick.bind(this);
-
   }
 
   handleClick(e){
@@ -26,18 +23,55 @@ class List extends Component {
       //this needs to be set and sort based on button click
     });
   }
+
+  componentWillMount(){
+    fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        'userName': responseJson[0].username,
+        'userIcon': responseJson[0].img,
+        'userPoints': responseJson[0].alltime,
+        'userPointsRecent': responseJson[0].recent
+      });
+    });
+
+  }
+
+
   renderUsers(i) {
-  return (
-    <User
-      userName={this.state.myUser[0].userName[i]}
-      userIcon={this.state.myUser[1].userIcon[i]}
-      userPoints={this.state.myUser[2].userPoints[i]}
-      userPointsRecent={this.state.myUser[3].userPointsRecent[i]}
-    />
-  );
-}
+  /*  let combined = [];
+    let name = this.state.userName.slice();
+    let icon = this.state.userIcon.slice();
+    let points = this.state.userPoints.slice();
+    let recentPoints = this.state.userPointsRecent.slice();
+    name.forEach((element, index) => {
+      combined.push({'userName': name[index], 'userIcon': icon[index], 'userPoints': points[index], 'userPointsRecent': recentPoints[index]});
+    });
+    combined.sort(function (a,b){
+      return b.userPoints - a.userPoints;
+    })
+    combined.forEach((element, index) => {
+      this.setState({
+        userName: combined[index].userName,
+        userIcon: combined[index].userIcon,
+        userPoints: combined[index].userPoints,
+        userPointsRecent: combined[index].userPointsRecent
+      })
+    });*/
+
+    return (
+      <User
+        userName={this.state.userName}
+        userIcon={this.state.userIcon}
+        userPoints={this.state.userPoints}
+        userPointsRecent={this.state.userPointsRecent}
+      />
+    );
+  }
 
   render() {
+
     return (
       <div>
         {Array.from(Array(5), (_, x) => this.renderUsers(x))}
