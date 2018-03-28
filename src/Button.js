efa6696d162afe29fb4ed5c422684e5b7b2c6c37
Ruth Ashleigh <notruth@notruth.com>
2018-03-28 16:50:51 -0700

    Converted to functional stateless component

--- src/Button.js
+++ src/Button.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import './bootstrap.min.css';
 
-export class MyButton extends Component{
-
-  render(){
+export function MyButton(props){
     return (
       <Grid>
         <Row className="alignButtons">
           <Col xs={12} sm={6} md={6} lg={6} xl={6}>
-            <Button id="allTime" onClick={this.props.onClick}>Sort by all time points</Button>
+            <Button id="allTime" onClick={props.onClick}>Sort by all time points</Button>
           </Col>
           <Col xs={12} sm={6} md={6} lg={6} xl={6}>
-            <Button id="recent" onClick={this.props.onClick}>Sort by recent points</Button>
+            <Button id="recent" onClick={props.onClick}>Sort by recent points</Button>
           </Col>
         </Row>
       </Grid>
     );
-  }
 }
